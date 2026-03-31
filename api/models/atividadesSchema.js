10f565457c2f83af048a3ce033356062396da59a
Marcos Daniel da Silva Lima <marcos.d.s.lima1408@gmail.com>
2026-03-31 19:31:55 -0300

    feat: Adicionando Schema para atividades & refact: Alterando docker-compose.yml para persistencia da pasta api/uploads/.

---
+++ api/models/atividadesSchema.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+
+export const atividadeSchema = new Schema({
+    titulo: { type: String, required: true },
+    descricao: { type: String, required: true },
+    disciplina: { type: String, required: true },
+    professor: {
+        nome: { type: String, required: true },
+        forma_envio: { 
+            type: String,
+            required: true,
+            enum: [ "E-mail", "Presencial", "Classroom", "Outro" ]
+        },
+        email: { 
+            type: String, 
+            required: function() { return this.professor.forma_envio === "E-mail"; } 
+        },
+        url: { 
+            type: String, 
+            required: function() { return this.professor.forma_envio === "Classroom"; } 
+        }
+    },
+    prazo: { type: Date, required: true },
+    status: { 
+        type: String,
+        enum: ["No prazo", "Atrasado", "Concluido", "Entregue"],
+        default: "No prazo"
+    },
+    anexo: { type: String },
+    criado: { type: Date, default: Date.now }
+});
