0107592b800693ad81d3614526242e7f47263962
Marcos Daniel da Silva Lima <marcos.d.s.lima1408@gmail.com>
2026-04-18 18:34:13 -0300

    refact: Adicionando tratamento de erro para rotas incorretas.

--- api/index.js
+++ api/index.js
@@ -8,12 +8,25 @@ app.use(express.json());
 
 import rootRouter from "./routes/rootRouter.js";
 app.use("/api", rootRouter);
+app.get('/', (req, res) => {
+    res.redirect('/api'); 
+});
 
 import atividadesRouter from "./routes/atividadesRouter.js";
 app.use("/api/atividades", atividadesRouter);
 
 import disciplinasRouter from "./routes/disciplinasRouter.js";
 app.use("/api/disciplinas", disciplinasRouter);
+
+// Tratamento para rotas não cadastradas
+app.use((req, res) => {
+    res.status(404).json({
+        status: 404,
+        error: "Rota não encontrada",
+        message: "Parece que você se perdeu. Acesse a documentação para ver as rotas disponíveis.",
+        docs: "http://127.0.0.1:3000/api"
+    });
+});
 
 const port = 3000;
 try {
